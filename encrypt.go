969d29637ffab671d31be2016981097b5fcb210d
ChaoticByte <contact@chaoticbyte.net>
2026-01-16 21:51:24 +0100

    Document the used encryption algorithms and code

--- encrypt.go
+++ encrypt.go
@@ -9,41 +9,63 @@ import (
 	"golang.org/x/crypto/chacha20poly1305"
 )
 
+/*
+
+The cipher used for encryption is XChaCha20-Poly1305, which is an AEAD algorithm.
+
+XChaCha20 is a ChaCha20 streaming cipher with a 24 byte nonce length.
+Poly1305 is the message authentication part (checks if the decrypted data is correct).
+
+No 'associated data' is written or read.
+
+The 32 byte key for encryption is derived using Argon2ID. A 12-byte random salt is used.
+
+*/
+
 const ErrMsgInvalidNonceLen = "Assembled nonce has an invalid length!"
 
 func EncryptText(password []byte, cleartext string, time uint64) ([]byte, [12]byte, [16]byte, error) {
+	// create random salt
 	salt := [12]byte{}
 	_, err := rand.Read(salt[:])
 	if err != nil { return []byte{}, salt, [16]byte{}, err }
+	// derive key
 	key := derive_key(password, salt)
+	// assemble nonce
 	noncePfx := [16]byte{}
 	_, err = rand.Read(noncePfx[:])
-	if err != nil { return []byte{}, salt, noncePfx, err }
-	aead, err := chacha20poly1305.NewX(key[:])
 	if err != nil { return []byte{}, salt, noncePfx, err }
 	nonce := []byte{}
 	nonce = append(nonce, noncePfx[:]...)
 	nonce = binary.BigEndian.AppendUint64(nonce, time)
+	// create aead cipher
+	aead, err := chacha20poly1305.NewX(key[:])
+	if err != nil { return []byte{}, salt, noncePfx, err }
+	// encrypt
 	src := []byte(cleartext)
 	dst := aead.Seal(nil, nonce, src, nil)
 	return dst, salt, noncePfx, err
 }
 
 func DecryptText(password []byte, ciphertext []byte, salt [12]byte, noncePfx [16]byte, time uint64) (string, error) {
+	// derive key
+	key := derive_key(password, salt)
+	// assemble nonce
 	nonce := []byte{}
 	nonce = append(nonce, noncePfx[:]...)
 	nonce = binary.BigEndian.AppendUint64(nonce, time)
 	if len(nonce) != 24 { return "", errors.New(ErrMsgInvalidNonceLen) }
-	key := derive_key(password, salt)
+	// create aead cipher
 	aead, err := chacha20poly1305.NewX(key[:])
 	if err != nil { return "", err }
+	// decrypt
 	dst, err := aead.Open(nil, nonce[:], ciphertext, nil)
 	if err != nil { return "", err }
 	result := string(dst)
 	return result, err
 }
 
-// internal
+// key derivation
 
 const a2_time = 10
 const a2_mem = 128*1024
